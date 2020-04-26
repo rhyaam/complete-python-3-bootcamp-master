9241ecf7bac26a3c4052a7aed8aa76009dc10748
Rohan Russell <rhyaam@gmail.com>
2020-04-26 08:19:51 -0400

    My First Terraform Commit

---
+++ Terraform Learning/main.tf
@@ -0,0 +1,66 @@
+provider "aws" {
+  region  = "us-east-1"
+  version = "~> 2.46"
+}
+
+resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_security_group" "http_server_sg" {
+  name = "http_server_sg"
+  #vpc_id = aws_default_vpc.default.id
+  vpc_id = vpc-2c805047
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name = "http_server_sg"
+  }
+}
+
+resource "aws_instance" "http_server" {
+  #ami                   = "ami-062f7200baf2fa504"
+  #ami                    = data.aws_ami.aws_linux_2_latest.id
+  ami                    = ami-0f7919c33c90f5b58
+  key_name               = "default-ec2"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+
+  //subnet_id              = "subnet-3f7b2563"
+  subnet_id = tolist(data.aws_subnet_ids.default_subnets.ids)[0]
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.aws_key_pair)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y",
+      "sudo service httpd start",
+      "echo Welcome to in28minutes - Virtual Server is at ${self.public_dns} | sudo tee /var/www/html/index.html"
+    ]
+  }
+}
